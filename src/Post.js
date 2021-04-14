dd24f0e360c2e259f4f5feb723102922fbdad868
luisaLuna <luisacasaslu@gmail.com>
2021-04-14 12:04:12 -0500

    Post CSS changes

--- src/Post.js
+++ src/Post.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './Post.css';
 import { Avatar } from "@material-ui/core";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import PublishIcon from "@material-ui/icons/Publish";
 
 function Post({
     displayName,
@@ -20,11 +25,22 @@ function Post({
                 <div className="post__header">
                     <div className="post__headerText">
                         <h3>
-                            Luisa Luna <span>
-                             <VerifiedUserIcon className="post__badge" />
+                            Luisa Luna {" "}
+                            <span className="post__headerSpecial">
+                             <VerifiedUserIcon className="post__badge" /> @luisaLuna
                             </span>
                         </h3>
                     </div>
+                    <div className="post__headerDescription">
+                        <p>Post a Tweet to try the app</p>
+                    </div>
+                </div>
+                <img src="https://media.giphy.com/media/XbxZ41fWLeRECPsGIJ/giphy.gif" alt=""/>
+                <div className="post__footer">
+                    <ChatBubbleOutlineIcon fontSize="small" />
+                    <RepeatIcon fontSize="small" />
+                    <FavoriteBorderIcon fontSize="small" />
+                    <PublishIcon fontSize="small" />
                 </div>
             </div>
         </div>
