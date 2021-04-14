c516e18db502747e521e2034d82478d1bcb23576
luisaLuna <luisacasaslu@gmail.com>
2021-04-14 12:30:31 -0500

    Widgets using TwitterTweetEmbed

--- src/Widgets.js
+++ src/Widgets.js
@@ -17,7 +17,19 @@ function Widgets() {
             
             <div className="widgets__widgetContainer">
                 <h2>What's happening</h2>
-                <TwitterTweetEmbed tweetId={"858551177860055040"} />
+
+                <TwitterTweetEmbed tweetId={"1380772975733923841"} />
+
+                <TwitterTimelineEmbed
+                    sourceType="profile"
+                    screenName="ycombinator"
+                    options={{ height: 400 }}
+                />
+
+                <TwitterShareButton
+                 url={"https://facebook.com/cleverprogrammer"}
+                 options={{ text: "#reactjs is awesome", via: "cleverqazi" }}
+                />
             </div>
         </div>
     )
