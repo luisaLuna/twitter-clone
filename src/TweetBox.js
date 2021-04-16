fce13258985822f7cfa49f9ee9affe56713158e4
luisaLuna <luisacasaslu@gmail.com>
2021-04-15 19:17:04 -0500

    Responsive

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -5,7 +5,7 @@ import db from "./firebase";
 
 function TweetBox() {
     const [tweetMessage, setTweetMessage] = useState('');
-    const [tweetImage, setTweetImage] = useState('');
+    // const [tweetImage, setTweetImage] = useState('');
 
     const sendTweet = (e) => {
         e.preventDefault();
@@ -15,12 +15,12 @@ function TweetBox() {
             username: "luisaLu",
             verified: true,
             text: tweetMessage,
-            image: tweetImage,
+            // image: tweetImage,
             avatar: "https://media-exp1.licdn.com/dms/image/C4E03AQHqBza5afsKwg/profile-displayphoto-shrink_800_800/0/1585851680521?e=1623888000&v=beta&t=f4ZKHlol3KMEZj36zWGumeSC071ptmKUQi6c3Rt6de8",
         });
 
         setTweetMessage('');
-        setTweetImage('');
+        // setTweetImage('');
     };
 
     return (
@@ -35,13 +35,13 @@ function TweetBox() {
                         placeholder="What's happening?"
                     />
                 </div>
-                <input
+                {/* <input
                     value={tweetImage}
                      onChange={e => setTweetImage(e.target.value)}
                     type="text"
                     className="tweetBox__imageInput"
                     placeholder="Optional: Enter image URL"
-                />
+                /> */}
                 <Button
                     onClick={sendTweet}
                     type="submit"
